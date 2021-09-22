4943c40c4061599f82b5d4d059eecd4df889dcb1
lynnsamuelson <lynn.samuelson@gmail.com>
2021-09-22 10:35:59 -0500

    added banners and links to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,47 +1,63 @@
-import React from "react"
+import React, {useState} from "react"
 import logo from  "../images/logo_long.png"
 import facebook from  "../images/facebook.png"
 import instagram from  "../images/instagram.png"
 import "./Footer.css"
 
-export const Footer = () => (
-  <div className="flex footer">
-    <section className="footer__img">
-      <img src={logo} alt="Logo" />
-      <div className="footer--social-media">
-        <img src={facebook} alt="Logo" />
-        <img src={instagram} alt="Logo" />
-      </div>
-    </section>
-    <section className="footer__section">
-        <p className="dark">Contact Us</p>
-        <p className="white">Email: info@finder.camp</p>
-        <p className="white">Sign up for the newsletter</p>
-    </section>
-    <section className="footer__section footer__section--help">
-        <p className="dark">Help</p>
-        <p className="white">Terms</p>
-        <p className="white">Privacy</p>
-        <p className="white">Ask a question</p>
-    </section>
-    <section className="footer__section">
-        <p className="dark">Camps</p>
-        <p className="white">Browse by camp</p>
-        <p className="white">Browse by category</p>
-        <p className="white">Browse by age group</p>
-        <p className="white">Browse by dates</p>
-    </section>
-    <section className="footer__section">
-      <button onClick={Login}>Create an account or sign in</button>
-      <button onClick={Search}>Find sessions</button>
-    </section>
-  </div>
-)
+export const Footer = () => {
+  const [showOverlay] = useState(true)
 
-const Login = () => {
-  console.log("need to login")
-}
+  const Login = () => {
+    console.log("need to login")
+  }
+  
+  const Search = () => {
+    console.log("need to search")
+  }
 
-const Search = () => {
-  console.log("need to search")
+  return (
+    <div className="flex footer">
+      <section className="footer__img">
+        <img src={logo} alt="Logo" />
+        <div className="footer--social-media">
+          <a href="https://facebook.com" target="_blank">
+            <img src={facebook} alt="Logo" />
+          </a>
+          <a href="https://instagram.com" target="_blank">
+            <img src={instagram} alt="Logo" />
+          </a>
+        </div>
+      </section>
+      <section className="footer__section">
+          <p className="dark">Contact Us</p>
+          <p className="white"><a href="mailto:info@finder.camp">Email: info@finder.camp</a></p>
+          <div className="newletter__overlay">
+            <p className="white">Sign up for the newsletter</p>
+            <div className="newsletter">
+              <h3 className="newsletterh3">Coming Soon!</h3>
+            </div>
+          </div>
+      </section>
+      <section className="footer__section footer__section--help">
+          <p className="dark">Help</p>
+          <p className="white">Terms</p>
+          <p className="white">Privacy</p>
+          <p className="white">Ask a question</p>
+      </section>
+      <section className="footer__section">
+          <p className="dark">Camps</p>
+          <p className="white">Browse by camp</p>
+          <p className="white">Browse by category</p>
+          <p className="white">Browse by age group</p>
+          <p className="white">Browse by dates</p>
+      </section>
+      <section className="footer__section">
+        <button onClick={Login}>Create an account or sign in</button>
+        <button onClick={Search}>Find sessions</button>
+        <div className="overlay">
+          <h3>Coming Soon!</h3>
+        </div>
+      </section>
+    </div>  
+  )
 }
